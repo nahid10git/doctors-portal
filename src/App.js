0632bf398ba2856488431323db21b51fd75660a2
nahid10git <nahidrashel@gmail.com>
2023-06-18 22:46:31 +0600

    navbar setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import router from './Routs/Routs/Routs';
 
 function App() {
   return (
-    <div>
+    <div className=' max-w-[1200px] mx-auto'>
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
