4a18e7472721d6982bd428e667d4f32dc60413eb
nahid10git <nahidrashel@gmail.com>
2023-06-18 13:01:18 +0600

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routs/Routs/Routs';
 
 function App() {
   return (
     <div>
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
